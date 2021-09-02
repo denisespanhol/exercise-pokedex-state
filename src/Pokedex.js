70d9c3ce7d5066d5bf08c40cab9fb50bd64a5680
Denis Espanhol <denisgfespanhol@gmail.com>
2021-09-02 20:56:58 -0300

    Exercício 1 do bloco 12 dia 1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
+  constructor() {
+    super()
+    this.nextPokemon = this.nextPokemon.bind(this);
+    this.comparePokemon = this.comparePokemon.bind(this);
+    
+    this.state = {
+      thisPokemon: 0,
     }
-}
+  }
+
+  nextPokemon() {
+    this.setState((prevState) => ({
+        thisPokemon: prevState.thisPokemon + 1,
+      }),
+    )}
+    
+  comparePokemon() {
+    if (this.state.thisPokemon < 8) {
+      this.nextPokemon()
+    } else {
+      this.setState(() => ({
+        thisPokemon: 0
+      }),)
+    }
+  }
+    
+    
+
+  render() {
+    const { pokemons } = this.props;
+    return (
+      <div className="pokedex">
+        {pokemons.filter((pokemon, index) => pokemons[index] === pokemons[this.state.thisPokemon] ).map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+        <button onClick={ this.comparePokemon }>Next!</button>
+      </div>
+    );
+  }
+};
 
 export default Pokedex;
